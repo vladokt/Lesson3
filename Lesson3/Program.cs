1ec39053ffb39e7f56ef481e791705ef33d5cb56
vladokt <vlad78tomsk@mail.ru>
2022-04-27 21:45:01 +0700

    day 1

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
+﻿namespace Lesson3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int arg1;
+            int arg2;
+            int result;
+            byte operationPos;
+            bool isOperationFound = false;
+            
+                            
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Enter expression (e.g. 3*56, 23/7, 5+8, 67-39, 6%5):");
+                string expression = Console.ReadLine();
+                string[] strargs = expression.Split('*', '/', '+', '-');
+
+
+
+
+                
+
+
+
+
+
+                Console.WriteLine("Another expression? (y/n)");
+            }
+            while (Console.ReadLine() == "y");
+            Console.WriteLine("Bye!");
+                       
+        }
+
+    }
+}
